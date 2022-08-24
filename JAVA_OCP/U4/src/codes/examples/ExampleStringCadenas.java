90e920943138806d852bf17310fcfe66a13f99e0
Chupendo <andresruizpenuela@gmail.com>
2022-08-24 09:24:08 +0200

    ejemplo de cadnas de string

---
+++ JAVA_OCP/U4/src/codes/examples/ExampleStringCadenas.java
@@ -0,0 +1,13 @@
+package codes.examples;
+
+public class ExampleStringCadenas {
+
+	public static void main(String[] args) {
+		String [] arrayString = {"One","Two","Three","For"};
+		
+		for(String str : arrayString) {
+			System.out.println(str);
+		}
+	}
+
+}
