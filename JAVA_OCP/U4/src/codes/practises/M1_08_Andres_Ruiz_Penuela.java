dfeb98da00a22bd4059d8159ad1782d4558e86a1
Chupendo <andresruizpenuela@gmail.com>
2022-08-29 17:12:59 +0200

    practicas unidad 4

--- JAVA_OCP/U4/src/codes/practises/M1_08_Andres_Ruiz_Penuela.java
+++ JAVA_OCP/U4/src/codes/practises/M1_08_Andres_Ruiz_Penuela.java
@@ -1,4 +1,7 @@
 package codes.practises;
+
+import java.util.Scanner;
+
 /**
  * Crear un programa que, dado un array de números enteros, determine cuales 
  * son sus elementos que se encuentran duplicados.
@@ -11,12 +14,59 @@ package codes.practises;
 public class M1_08_Andres_Ruiz_Penuela {
 
 	public static void main(String[] args) {
-		int[] arrayDePrueba= {1, 2, 3, 3, 9, 8, 7, 4, 6, 7, 0, 4, 5, 3};
-		short[] duplicados = new short[1], temporal= new short[1];
-		short count=0;
+		Scanner sc = new Scanner(System.in);
+		short[] arrayDePrueba = new short[1];//= {1, 2, 3, 3, 9, 8, 7, 4, 6, 7, 0, 4, 5, 3};
+		short duplicados[]  = new short[1], temporal[]= new short[1], count=0, numero;
+		String temp;
 		boolean agregar = false;
 		
+		//Solicitamos los numeros
 		
+		do {
+			System.out.print("Inserte un nunemro (. para salir): ");
+			temp = sc.nextLine();
+			if(temp.equals("."))
+				break;
+			try {
+				numero = Short.parseShort(temp);
+				
+				if(temporal==null){//No es la primera iteracion
+					temporal = arrayDePrueba.clone();
+					arrayDePrueba = new short[temporal.length+1];
+					for(short elemento :temporal) {
+						arrayDePrueba[count]=elemento;
+						count++;
+					}
+				}else {
+					arrayDePrueba = new short[1];
+				}
+					
+				// Volcamos el dato en el array
+				arrayDePrueba[count] = numero;
+				
+				// Resetemaos
+				numero = 0;
+				temporal = null;
+				count = 0;
+				
+			}catch(Exception ex) {
+				System.err.println("No es un numero: "+ex);
+			}
+		}while(true);
+		
+		// Resetemaos
+		numero = 0;
+		temporal = null;
+		count = 0;
+		// Mostramos el vector
+		System.out.print("Vector: { ");
+		for(short elem : arrayDePrueba) {
+			System.out.print(elem+" ");
+		}
+		System.out.println("}");
+		
+		
+		// Buscamos repetidos
 		for(int i=0;i<arrayDePrueba.length;i++) {
 			for(int j=i+1;j<arrayDePrueba.length;j++) {
 				
@@ -59,11 +109,15 @@ public class M1_08_Andres_Ruiz_Penuela {
 		}
 
 		// Mostramos resultados
-		System.out.print("Repetido/s: { ");
-		for(short elem : duplicados) {
-			if(elem!=-1)
-			System.out.print(elem+" ");
+		if(count==0)
+			System.out.println("No hay elementos repetidos");
+		else {
+			System.out.print("Repetido/s: { ");
+			for(short elem : duplicados) {
+				if(elem!=-1)
+				System.out.print(elem+" ");
+			}
+			System.out.println("}");
 		}
-		System.out.println("}");
 	}
 }
