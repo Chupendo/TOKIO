d9c1317d3c4c2be77be67d9829eef5883a17dbeb
Chupendo <andresruizpenuela@gmail.com>
2022-09-11 17:05:00 +0200

    ejericio 6 unid 6, book java

---
+++ JAVA_OCP/U5/src/codes/exercicies/tokio/exercicies/three/StringInv.java
@@ -0,0 +1,24 @@
+package codes.exercicies.tokio.exercicies.three;
+
+public class StringInv {
+
+	public static void main(String[] args) {
+		String txt = "Andres";
+		
+		System.out.println("txt: "+txt);
+		System.out.print("txt inv: ");
+		showStringInv(txt);
+
+	}
+	
+	public static void showStringInv(String txt) {
+		showStringInv(txt,txt.length()-1);
+	}
+	public static void showStringInv(String txt,int pos) {
+		if(pos>=0) {
+			System.out.print(txt.charAt(pos));
+			showStringInv(txt,pos-1);
+		}
+					
+	}
+}
