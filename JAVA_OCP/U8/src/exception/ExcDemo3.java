ea575626cd8972f21f21c9c24b587e2a594068c0
Chupendo <andresruizpenuela@gmail.com>
2022-11-23 21:25:28 +0100

    lanzar excepciones

---
+++ JAVA_OCP/U8/src/exception/ExcDemo3.java
@@ -0,0 +1,28 @@
+package exception;
+
+import java.util.function.Function;
+
+// Control de errores con elegancia
+public class ExcDemo3 {
+	private static Function<Integer, Integer> fun1 = (a) -> (int) Math.round(Math.pow(a, 2));
+	public static void main(String[] args) {
+		int number[] = new int[6];
+		int denom[] = {2,0,4,4,0,8};//new int[number.length];
+		for(int i=0;i<number.length;i++) {
+			if(i==0)
+				number[i] = fun1.apply(2);
+			else
+				number[i] = fun1.apply(number[i-1]);
+		}
+		
+		// Division number/denom
+		for (int i=0; i<number.length; i++) {
+			try {
+				System.out.println(number[i] + " / " + denom[i] +" is "+number[i]/denom[i]);
+			}catch(ArithmeticException exc) {
+				// capturar la excepcion
+				System.out.println("Can't divide by Zero!");
+			}
+		}
+	}
+}
---
+++ JAVA_OCP/U8/src/exception/ExcDemo5.java
@@ -0,0 +1,16 @@
+package exception;
+
+import java.util.function.Function;
+
+// Lanzar una excepción
+public class ExcDemo5 {
+	public static void main(String[] args) {
+		try {
+			System.out.println("Before throw.");
+			throw new ArithmeticException("Error");
+		} catch (ArithmeticException e) {
+			e.printStackTrace();
+		}
+		System.out.println("After throw");
+	}
+}
