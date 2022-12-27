f6341d06594544529ceeb15d41e26762e245280b
Chupendo <andresruizpenuela@gmail.com>
2022-12-27 15:49:15 +0100

    ejemplo de export ... to ...

--- JAVA_OCP/Modulos/appfuncs/src/module-info.java
+++ JAVA_OCP/Modulos/appfuncs/src/module-info.java
@@ -1,5 +1,5 @@
 // Definición de módulo para el módulo de funciones
 module appfuncs{
 	// Exporta el paquete appfuncs.simplefuncs
-	exports appfuncs.simplefuncs;
+	exports appfuncs.simplefuncs to appstart; // Exportación restringida
 }
